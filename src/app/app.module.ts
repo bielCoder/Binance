0a7562188f38d8fbbd961dfd47253563b31601cc
Gabriel Henrique Carvalho de Sousa <gabriel.sousa@gpnet.com.br>
2023-04-16 18:45:11 -0300

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { HomeComponent } from './components/pages/home/home.component';
+import { BenefitsComponent } from './components/pages/benefits/benefits.component';
+import { ResourcesComponent } from './components/pages/resources/resources.component';
+import { PricesComponent } from './components/pages/prices/prices.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    BenefitsComponent,
+    ResourcesComponent,
+    PricesComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
